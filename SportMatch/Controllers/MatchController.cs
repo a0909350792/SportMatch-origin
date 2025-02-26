406647680a278204e5b98b7d8d5d4a567eb84bd3
One-Lin <asd147267@gmail.com>
2025-02-26 16:37:33 +0800

    20250226新增PartialView

--- SportMatch/Controllers/MatchController.cs
+++ SportMatch/Controllers/MatchController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportMatch.Models;
 
 namespace SportMatch.Controllers
 {
@@ -6,6 +7,23 @@ namespace SportMatch.Controllers
     {
         public IActionResult MatchPage()
         {
+            TestForMatch Player1 = new TestForMatch();
+            Player1.Name = "妙蛙種子";
+            Player1.Role = "控球後衛";
+            Player1.Image = "/image/MatchPage/001.png";
+
+            List<TestForMatch> Player2 = new List<TestForMatch>
+    {
+        new TestForMatch{Name="妙蛙種子",Role="控球後衛",Image="./image/MatchPage/001.png"},
+        new TestForMatch{Name="妙蛙草", Role="大前鋒", Image="./image/MatchPage/002.png"},
+        new TestForMatch{Name="妙蛙花", Role="中鋒", Image="./image/MatchPage/003.png"},
+        new TestForMatch{Name="小火龍", Role="小前鋒",Image="./image/MatchPage/004.png"},
+        new TestForMatch{Name="火恐龍", Role="得分後衛",Image="./image/MatchPage/005.png"},
+        new TestForMatch{Name="噴火龍", Role="中鋒", Image="./image/MatchPage/006.png"}
+            };
+
+
+            ViewData["123"] = Player1;
             return View();
         }
     }
---
+++ SportMatch/Models/TestForMatch.cs
@@ -0,0 +1,9 @@
+﻿namespace SportMatch.Models
+{
+    public class TestForMatch
+    {
+        public string? Name { get; set; }
+        public string? Role { get; set; }
+        public string? Image { get; set; }
+    }
+}
