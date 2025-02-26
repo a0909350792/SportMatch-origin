fb679d2f2f185d62bb151d23d47cac07d1dc02a0
One-Lin <asd147267@gmail.com>
2025-02-26 13:53:51 +0800

    分支建立，bootstrap版本問題待修

--- SportMatch/Program.cs
+++ SportMatch/Program.cs
@@ -68,6 +68,6 @@ app.UseEndpoints(endpoints =>
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Match}/{action=MatchPage}/{id?}");
 
 app.Run();
